b60c0826bdd2c7de2e8ce8b4191a37bdfaca53c9
Rahul Gupta <rahul@gmail.com>
2026-09-11 13:48:09 +0530

    Arrow function

---
+++ This_and_arrow_Function.js
@@ -0,0 +1,26 @@
+// THIS FUnction in js
+
+const user ={
+    name: "rahul",
+    price : 999,
+
+    welcomMsg : function(){
+        console.log(`Hello ${this.name} , Welcome TO My Website`);
+        
+    }
+ }
+// user.welcomMsg()
+// user.name = "sam"
+// user.welcomMsg()
+
+// Arrow Function in js====>
+
+// syntax ==>  () =>{}
+
+    // const addNums = (num1,num2) =>{
+    //     return num1 + num2
+    // }
+    // console.log(addNums(4,5));
+    
+    // const n = (num1, num2) =>  num1 + num2    // Implisit Arrow function
+    // const n = (num1, num2) =>  (num1 + num2 )
