91c7114800dc3ea5c73339ac82cb9794a7768805
Rahul Gupta <rahul@gmail.com>
2026-09-16 15:46:35 +0530

    For Loop IN JS

---
+++ Loops.js
@@ -0,0 +1,57 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+ if (element == 5){
+    // console.log("5 is best number");
+    
+ }    
+//  console.log(element);
+ 
+}
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("Outer loop");
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`Inner loop Value ${j} and Outer Loop is ${i}`);
+//             console.log(i + "*" + j + "="+ i*j);
+                
+//     }
+    
+// }
+
+// const Myarray = ["Batman","Superman", "DC","Marvels"]
+// console.log(Myarray.length);
+
+// for (let i = 0; i < Myarray.length; i++) {
+//     const element = Myarray[i];
+//     console.log(element);
+    
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i == 5){
+//         console.log("Detected 5");
+//         break
+    
+//     }
+//    console.log(i);
+       
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i == 5){
+//         console.log("Detected 5");
+//         continue
+    
+//     }
+//    console.log(i);
+       
+// }
