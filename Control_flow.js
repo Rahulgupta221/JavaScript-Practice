dfc37397df17c92b55f3f27d51e583c01462a4ac
Rahul Gupta <rahul@gmail.com>
2026-09-16 12:54:19 +0530

    Operators in JS

---
+++ Control_flow.js
@@ -0,0 +1,102 @@
+// here im going to learn Operators===============>
+
+// Operators are ==> <,>,<=,>=,==,!=,===,!==
+
+// if(2 >= 4){
+//     console.log("True");
+    
+// }else{
+//     console.log("false");
+    
+// }
+
+// const bal = 1000
+
+// if(bal >500){
+//     console.log("Less then 500");
+    
+// }else if(bal >= 750){
+//     console.log("Less then 750");
+    
+// }else{
+//     console.log(bal >1200);
+    
+// }
+
+
+// ==================Switch=====================>
+
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+        
+//         break;
+//     case 2:
+//         console.log("Feburary");
+        
+//         break;
+//     case 3:
+//         console.log("March");
+        
+//         break;
+//     case 4:
+//         console.log("April");
+        
+//         break;
+
+//     default:
+//         console.log("Default value");
+        
+//         break;
+// }
+
+
+const userLoggedin = []
+
+if(userLoggedin){
+console.log("Got logged in user");
+
+} else {
+    console.log("Don't logged  in user");
+    
+}
+
+// Falsy Values
+
+// false, 0, -0, BigInt 0n, "" , null , undefined , NaN 
+
+// Truthy values
+
+// "0", "false"," ", [], {}, function(){}
+
+
+// if (userLoggedin.length ===0 ){
+//     console.log("Array is Empty");
+    
+// }
+
+
+// const emptyobj = {}
+
+// if (Object.keys(emptyobj).length === 0){
+// console.log("Object is empty");
+
+// }
+
+
+// Nullish Coalescing Operator (??): null undefine
+
+// const val1= 5 ?? 10
+// const val1= 10 ?? null
+// const val1= undefined ?? 10
+
+// console.log(val1);
+
+// Ternory Operators===>
+
+// const val = 100
+// val <=80 ? console.log("Less then 80") : console.log("more than 80");
+
