916d59062b872b16c02423bc8bb0b33fd6233a04
Rahul Gupta <rahul@gmail.com>
2026-09-18 10:55:09 +0530

    While Loops in Js

---
+++ While_Loops.js
@@ -0,0 +1,27 @@
+// let i = 0
+// while (i <=10) {
+//     console.log(i);
+    
+//     i = i + 2
+// }
+
+
+// let Myarr = ["batman","Superman","Marvel"]
+// let arr = 0
+// while (arr < Myarr.length) {
+// console.log(`Value is ${Myarr[arr]}`);
+// arr = arr + 1
+  
+// }
+
+
+// Do - while=====>
+
+    // let score = 1
+
+    // do {
+    //     console.log(score);
+    //     score ++
+        
+    // } while (score <= 10);
+    
