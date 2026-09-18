8411b674dc0906be40a143a404d4944f3beb2438
Rahul Gupta <rahul@gmail.com>
2026-09-18 13:29:28 +0530

    High_order_ Arrays_Loops

---
+++ High_order_loops.js
@@ -0,0 +1,53 @@
+// For of====>
+
+// const array =["","",""]
+// let aarr = [{},{},{}]
+
+// Example Q1
+
+// const val = [1,2,3,4,5,6]
+
+// for (const nums of val) {
+//     console.log(nums);
+    
+// }
+
+// Ex Q2
+
+// const arrName = "Hello world"
+// for (const greet of arrName) {
+//     console.log(`Each char is ${greet}`);
+    
+// }
+
+
+// Map Function =====>
+
+// const map = new Map()
+// map.set("IN","India")
+// map.set("US","America")
+// map.set("Fr","France")
+// map.set("IN","India")
+
+// // console.log(map);
+
+// for (const key of map) {
+//  console.log(key);
+    
+// }
+
+
+// For in Function ===>
+
+// let myobj ={
+//     js: "script",
+//     cpp: "c++",
+//     rb:"ruby",
+//     py:"python"
+// }
+
+// for (const key in myobj) {
+
+//     console.log(key);
+    
+// }
